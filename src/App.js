5bb16adf662cb453e0d3e8a6a09f57c59213e750
anikasethi15 <anikasethi1512@gmail.com>
2025-11-01 15:05:55 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React + Docker Multi-Stage Build Demo</h1>
+      <p>This app was built using a Docker multi-stage build setup!</p>
+    </div>
+  );
+}
+
+export default App;
